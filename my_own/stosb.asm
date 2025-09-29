8de4878dfbb4f1bf0870fcd435ccf98fdaf07f5d
Orest Chumak <orest.chumak@gmail.com>
2025-09-29 22:55:33 +0300

    Add another way use stosb (reserved data in .bss)

--- my_own/stosb.asm
+++ my_own/stosb.asm
@@ -3,11 +3,12 @@ section .text
 
 _start:
 
+    ; ### stack as buffer ###
     sub esp, 10     ; reserve stack
-    
     cld
     lea edi, [esp]  ; in current situation [esp] it's our [buffer]
-    mov al, 49      ; value
+    mov al, '1'     ; value
+    xor ecx, ecx    ; clear ecx register
     mov cl, 10      ; repeat times
     rep stosb
 
@@ -17,9 +18,26 @@ _start:
     mov ecx, esp
     mov edx, 10
     int 0x80
+    
+    add esp, 10     ; clear stack after all
 
-    ; pre-exit
-    add esp, 10     ; clear stack
+    ; ### reserved data as buffer ###
+    cld
+    lea edi, [buffer]
+    mov al, '2'     ; value
+    xor ecx, ecx    ; clear ecx register
+    mov cl, 10      ; repeat times
+    rep stosb
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, buffer
+    mov edx, 10     
+    int 0x80
+    
     ; exit
     mov eax, 1
     int 0x80
+
+section .bss
+    buffer: resb 10

a5f514f91cf4f937b2ecbcce63dad0a0faccb9d6
Orest Chumak <orest.chumak@gmail.com>
2025-10-02 14:46:39 +0300

    Fix: converting '0' value now works

--- homework/hw2.asm
+++ homework/hw2.asm
@@ -6,20 +6,31 @@ section .text
     global _start
 
 _start:
-    mov [buffer_ptr], byte 0   ; init buffer busy bytes (0)
     mov [num_len], byte 0
     
     ; todo: where to save???
 
-    mov eax, 1234567890     ; our value/parameter
+    mov eax, 123456     ; our value/parameter
     call intToStr       ; call convertor
-    
 
-    ; exit
+exit:
+    ; print result
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, buffer
+    mov edx, [num_len]
+    int 0x80
+    
     mov eax, 1
     int 0x80
 
 intToStr:
+    cmp eax, 0
+    jne continue
+    mov [buffer], byte '0'
+    inc byte [num_len]
+    jmp exit
+continue:
     xor ecx, ecx    ; clear the ecx register
     mov cl, 1
     sub esp, 15
@@ -43,40 +54,20 @@ a:
     div ebx
     mov [esp + 10], eax              ; save new value
 
-    ; debug
-    ; mov edi, edx
-    ; call printNum
-
-    add dl, byte '0'
+    add dl, byte '0'    ; convert into ascii
     
-    ; ### save int as char[n].
-; other way make it
+    ; save in stack converted value
     mov eax, [num_len]
-    ; mov eax, BUFFER_MAX_LEN     ; write from end
-    ; sub eax, 1
-    ; sub eax, [num_len]
     mov [esp + eax], byte dl
     inc byte [num_len]
 
-    ; mov eax, [num_len]
-    ; call printNum2
-
-    ;
-    ; debug: print [esp + eax]
-    ; xor ebx, ebx
-    ; mov bl, byte [esp + eax]
-    ; xor eax, eax
-    ; mov al, dl
-    ; sub al, '0'
-    ; call printNum2
-    
     
     xor ecx, ecx
     mov cl, byte [esp + 14]      ; recover iter
     loop l1
 l1_ex:  ; } end loop
 
-
+    ; save in buffer
     mov cl, [num_len]
 l2:
     mov [esp + 14], byte cl      ; save iter
@@ -86,32 +77,20 @@ l2:
     xor edx, edx
 
     mov al, cl      ; last pos
-    sub al, 1
+    dec al
 
     mov bl, [num_len]
-    sub bl, 1
+    dec bl
     sub bl, al
     xor ecx, ecx
     mov cl, byte [esp + ebx]    ; get symbol
 
-    mov [buffer + eax], byte cl
-
-    ;
-    ; mov al, [buffer + eax]
-    ; sub al, '0'
-    ; call printNum2
+    mov [buffer + eax], byte cl ; save symbol in buffer
 
     xor ecx, ecx
     mov cl, byte [esp + 14]      ; load iter
     loop l2
 l2_ex:
-;     ; print result
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, buffer
-    mov edx, [num_len]
-    int 0x80
-
     add esp, 15      ; free stack
     ret
 
@@ -128,19 +107,6 @@ printNum:
 
     pop edi
     ret
-
-printNum2:
-    add eax, dword '0'
-    push eax
-
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, esp
-    mov edx, 1
-    int 0x80
-
-    pop eax
-    ret
 ; ##### data section #####
 
 section .data
@@ -148,5 +114,4 @@ section .data
     
 section .bss
     buffer: resb BUFFER_MAX_LEN
-    buffer_ptr: resb 1  ; save position the same way as esp: add --> free space
     num_len: resb 1

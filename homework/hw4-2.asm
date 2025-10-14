91a2a5948fd94396771a594b49084f29974ccc57
Orest Chumak <orest.chumak@gmail.com>
2025-10-14 23:38:14 +0300

    Finished hw4-2 to release

--- homework/hw4-2.asm
+++ homework/hw4-2.asm
@@ -1,16 +1,14 @@
 ; ### area of extern functions/procedures ###
 extern printInt
 extern printLF
-extern printSym
-extern printPrimeNum
 
 ; ### base ###
 section .text
     global _start
 
 _start:
-    xor     eax, eax    ; clear register
-    mov     ax,  word 11 ; our source number
+    xor     eax, eax        ; clear register
+    mov     ax,  word 12    ; our source number
     
     ; print our value
     mov     esi, eax
@@ -24,7 +22,6 @@ _start:
     ; glue results in one register (dx:ax --> eax)
     shl     edx, 16     ; 0x0000FFFF --> 0xFFFF0000
     or      eax, edx
-    
     ; print result
     mov     esi, eax
     call    printInt
@@ -35,21 +32,25 @@ _start:
     int     0x80
 
 ; fn factorial(ax: source-value) -> dx:ax
+; // This function is used to pass the required parameters into "factorial_rec"
+; // more comfortable - don't think about DX and CX registers
 factorial:
-    mov dx, 0
-    cmp ax, 1
-    jna ne
-    mov cx, ax
-    dec cx      ; iter = ax - 1
-    call factorial_rec  ; return (dx:ax)
+    mov     dx, 0
+    ; skip factorial calculation of 1 or 0
+    cmp     ax, 1
+    jna     ne
+    mov     cx, ax
+    dec     cx              ; iter = ax - 1
+    call    factorial_rec   ; return (dx:ax)
 ne:
     ret
 
+; fn factorial((dx:ax): source-value) -> dx:ax
 factorial_rec:
     sub     esp, 10
     ;       [esp + 8], word 0   ; dx buffer
     ;       [esp + 6], word 0   ; ax buffer
-    mov     [esp + 4], word dx   ; dx after mul
+    mov     [esp + 4], word dx  ; dx after mul
     mov     [esp + 2], word ax  ; ax after mul
     mov     [esp    ], cx       ; iter (num/cx)
 
@@ -65,7 +66,6 @@ factorial_rec:
     mov     ax,  word [esp + 2]
     jmp     finish
 next_iter:
-
     ; # AX mul
     mov     ax, [esp + 2]   ; --:ax
     mov     bx, [esp]
@@ -81,9 +81,9 @@ next_iter:
     ; add the pre-result for getting a full result
 
     ; final result
-    mov dx, ax
-    add dx, [esp + 8]
-    mov ax, [esp + 6]
+    mov     dx, ax
+    add     dx, [esp + 8]
+    mov     ax, [esp + 6]
 
     ; !!! ---------------- !!!
     ; For future updates!
@@ -91,39 +91,12 @@ next_iter:
     ; These are the parameters for the next recursion iteration
     ; ------------------------
     
-    ; load results
-    ; mov     dx, [esp + 8]
-    ; mov     ax, [esp + 6]
-
-    ; add ax, dx
-    ; mov dx, 0
-
     ; ps: potential place for free stack : will save much stack memory
     ; > sub     esp, 10
-
+    
     mov     cx, word [esp]
     dec     cx
     call    factorial_rec
 finish:
     add     esp, 10
-    ret
-
-; -----------------------------
-
-; Приклад виклику:
-; push word 5
-; call countdown_rec
-; add esp, 2 ; Очистити стек від параметра
-
-countdown_rec:
-    sub esp, 2
-    mov [esp], word ax
-
-    cmp     ax, 0
-    je      fini
-    mov     ax, word [esp]
-    dec     ax
-    call    countdown_rec
-fini:
-    add esp, 2
     ret

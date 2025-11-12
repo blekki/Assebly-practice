1892f699fb51cca60949cbf37f60c937f2836544
Orest Chumak <orest.chumak@gmail.com>
2025-11-12 17:11:27 +0200

    Clean and prepare code to release

--- homework/hw6.asm
+++ homework/hw6.asm
@@ -9,209 +9,214 @@ section .text
     global _start
 
 _start:
+    ; clear registers
+    xor     ebx, ebx
+    xor     ecx, ecx
 
-    xor ebx, ebx
-    xor ecx, ecx
+    ; sorting
+    mov     esi, source
+    mov     edi, destination
+    mov     bx, 2               ; num len (1, 2 or 4 bytes)
+    mov     cx, 10              ; elem count
+    call    printArray          ; print unsorted data
+    call    sort                ; sort array
+    ;
+    mov     esi, edi
+    call    printArray          ; print result
 
-    mov esi, source
-    mov edi, destination
-    mov bx, 4
-    mov cx, 10
-    call printArray     ; print unsorted data
-    call sort           ; sort
-    mov esi, edi
-    call printArray     ; print sorted data
+    ; exit
+    mov     eax, 1
+    int     0x80
 
-    mov eax, 1
-    int 0x80
-
+; <> fn explanation <>
 ; esi - source
 ; edi - drain
-; cx - elem count
-; bx - symbol size
+; cx  - elem count
+; bx  - symbol size
 sort:
-    push bx
-    push cx
-    sub esp, 12
-    mov [esp + 10], word 0    ; min value address (offset)
-    mov [esp +  8], word 0    ; offset (l_2)
-    mov [esp +  6], word 0    ; offset (l_1)
-    mov [esp +  4], cx   ; (iter l_2)
-    mov [esp +  2], cx   ; (iter l_1)
-    mov [esp     ], bx   ; elem size
+    push    bx  ; # note: <push> for showing number won't be change
+    push    cx
+    sub     esp, 12
+    mov     [esp + 10], word 0  ; min value address (offset)
+    mov     [esp +  8], word 0  ; offset (l_2)
+    mov     [esp +  6], word 0  ; offset (l_1)
+    mov     [esp +  4], cx      ; iter (l_2)
+    mov     [esp +  2], cx      ; iter (l_1)
+    mov     [esp     ], bx      ; elem size
 
     ; copy source into destination
-    push esi    ; save origin addresses
-    push edi
-    mov ecx, source_byte_len
+    push    esi    ; save origin addresses
+    push    edi
+    mov     ecx, source_byte_len
     cld
-    rep movsb   ; copy esi into edi
-    pop edi     ; recover addresses
-    pop esi
+    rep     movsb   ; copy esi into edi
+    pop     edi     ; recover addresses
+    pop     esi
 
-; ### use algorithm to the everyone elem
-    mov cx, [esp + 2]
+; ### sorting
+    mov     cx, [esp + 2]
 l_1:
-    mov [esp + 2], cx   ; save iter
+    mov     [esp + 2], cx   ; save iter
 
     ; prepare
-    mov ax, [esp + 6]
-    mov [esp + 8], ax   ; set l_2 offset
-    mov [esp + 10], ax  ; reset min value address
+    mov     ax, [esp + 6]
+    mov     [esp + 8], ax   ; set l_2 offset
+    mov     [esp + 10], ax  ; reset min value address
 
-; ### find minimum
-    mov cx, [esp + 2]   ; start with (l_1) iter value
+; ### find minimum value
+    mov     cx, [esp + 2]   ; start with (l_1) iter value
 l_2:
-    mov [esp + 4], cx   ; save iter
+    mov     [esp + 4], cx   ; save iter
 
-    ; compare two values (current and min)    ; todo: compare 2,4,8 bytes value
-    xor eax, eax
-    xor ebx, ebx
-    xor ecx, ecx
-    xor edx, edx
+    ; compare two values (current and min)
+    xor     eax, eax
+    xor     ebx, ebx
+    xor     ecx, ecx
+    xor     edx, edx
 
     ; copy bytes
-    mov cx, [esp + 8]   ; num A offse (current num)
-    mov dx, [esp + 10]  ; num B offse (last min)
+    mov     cx, [esp + 8]   ; num A offse (current num)
+    mov     dx, [esp + 10]  ; num B offse (last min)
     ;
-    cmp word [esp], 1   ; copy 1 byte
-    je copy_byte
-    cmp word [esp], 2   ; copy 2 bytes
-    je copy_word
-    cmp word [esp], 4   ; copy 4 bytes
-    je copy_dword
+    cmp     word [esp], 1   ; copy 1 byte
+    je      copy_byte
+    cmp     word [esp], 2   ; copy 2 bytes
+    je      copy_word
+    cmp     word [esp], 4   ; copy 4 bytes
+    je      copy_dword
 copy_byte:
-    mov al, [destination + ecx]
-    mov bl, [destination + edx]
-    jmp copy_done
+    mov     al, [destination + ecx]
+    mov     bl, [destination + edx]
+    jmp     copy_done
 copy_word:
-    mov ax, [destination + ecx]
-    mov bx, [destination + edx]
-    jmp copy_done
+    mov     ax, [destination + ecx]
+    mov     bx, [destination + edx]
+    jmp     copy_done
 copy_dword:
-    mov eax, [destination + ecx]
-    mov ebx, [destination + edx]
+    mov     eax, [destination + ecx]
+    mov     ebx, [destination + edx]
 copy_done:
 
-    cmp eax, ebx  ; if (A[i] < min) save new min value
-    ja cont    ; if above -> do nothing
-    mov ax, [esp + 8]   ; get current pos
-    mov [esp + 10], ax  ; save min value pos in dest
+    cmp     eax, ebx        ; if (A[i] < min) save new min value
+    ja      cont            
+    mov     ax, [esp + 8]   ; get current pos
+    mov     [esp + 10], ax  ; save min value pos in dest
 cont:
     ; next elem address offset (l_2)
-    mov ax, [esp]
-    add [esp + 8], ax
+    mov     ax, [esp]
+    add     [esp + 8], ax
     ; recover iter
-    xor ecx, ecx
-    mov cx, [esp + 4]
-    dec cx
-    jnz l_2
+    xor     ecx, ecx
+    mov     cx, [esp + 4]
+    dec     cx
+    jnz     l_2
 ; end loop (l_2)
 
     ; swap bytes
-    xor edx, edx
-    cmp word [esp], 1   ; copy 1 byte
-    je swap_byte
-    cmp word [esp], 2   ; copy 2 byte
-    je swap_word
-    cmp word [esp], 4   ; copy 4 byte
-    je swap_dword
+    xor     edx, edx
+    cmp     word [esp], 1   ; copy 1 byte
+    je      swap_byte
+    cmp     word [esp], 2   ; copy 2 byte
+    je      swap_word
+    cmp     word [esp], 4   ; copy 4 byte
+    je      swap_dword
 swap_byte:
-    mov dx, [esp + 10]
-    mov al, [destination + edx] ; save min (min -> A)
-    mov dx, [esp + 6]
-    mov bl, [destination + edx] ; save cur (cur -> B)
+    mov     dx, [esp + 10]
+    mov     al, [destination + edx] ; save min (min -> A)
+    mov     dx, [esp + 6]
+    mov     bl, [destination + edx] ; save cur (cur -> B)
     ;
-    mov dx, [esp + 10]
-    mov [destination + edx], bl ; B -> min
-    mov dx, [esp + 6]
-    mov [destination + edx], al ; A -> cur
-    jmp swap_done
+    mov     dx, [esp + 10]
+    mov     [destination + edx], bl ; load B (B -> min)
+    mov     dx, [esp + 6]
+    mov     [destination + edx], al ; load A (A -> cur)
+    jmp     swap_done
 swap_word:
-    mov dx, [esp + 10]
-    mov ax, [destination + edx]
-    mov dx, [esp + 6]
-    mov bx, [destination + edx]
+    mov     dx, [esp + 10]
+    mov     ax, [destination + edx]
+    mov     dx, [esp + 6]
+    mov     bx, [destination + edx]
     ;
-    mov dx, [esp + 10]
-    mov [destination + edx], bx
-    mov dx, [esp + 6]
-    mov [destination + edx], ax
-    jmp swap_done
+    mov     dx, [esp + 10]
+    mov     [destination + edx], bx
+    mov     dx, [esp + 6]
+    mov     [destination + edx], ax
+    jmp     swap_done
 swap_dword:
-    mov dx, [esp + 10]
-    mov eax, [destination + edx]
-    mov dx, [esp + 6]
-    mov ebx, [destination + edx]
+    mov     dx, [esp + 10]
+    mov     eax, [destination + edx]
+    mov     dx, [esp + 6]
+    mov     ebx, [destination + edx]
     ;
-    mov dx, [esp + 10]
-    mov [destination + edx], ebx
-    mov dx, [esp + 6]
-    mov [destination + edx], eax
+    mov     dx, [esp + 10]
+    mov     [destination + edx], ebx
+    mov     dx, [esp + 6]
+    mov     [destination + edx], eax
 swap_done:
 
     ; next elem address offset (l_1)
-    mov ax, [esp]
-    add [esp + 6], ax
+    mov     ax, [esp]
+    add     [esp + 6], ax
     ; recover iter
-    xor ecx, ecx
-    mov cx, [esp + 2]
-    dec cx
-    jnz l_1
+    xor     ecx, ecx
+    mov     cx, [esp + 2]
+    dec     cx
+    jnz     l_1
 ; end loop (l_1)
 
     ; final actions
-    add esp, 12
-    xor ecx, ecx
-    xor ebx, ebx
-    pop cx
-    pop bx
+    add     esp, 12
+    xor     ecx, ecx
+    xor     ebx, ebx
+    pop     cx
+    pop     bx
     ret
 
-printArray: ; fn printArray(source: esi) -> void
-    push esi    ; # note: <push> for showing the immutability of a number
-    push edi
-    push ebx
-    push ecx
-    sub esp, 12
-    mov [esp + 8], esi      ; source
-    mov [esp + 4], ebx      ; num size (in bytes)
-    mov [esp    ], ecx      ; iter/len   (changeable)
+; fn printArray(source: esi) -> void
+printArray:
+    push    esi     ; # note: <push> for showing number won't be change
+    push    edi
+    push    ebx
+    push    ecx
+    sub     esp, 12
+    mov     [esp + 8], esi      ; source
+    mov     [esp + 4], ebx      ; num size (in bytes)
+    mov     [esp    ], ecx      ; iter/len   (changeable)
     
 l_3:
-    mov [esp], ecx   ; save iter
+    mov     [esp], ecx      ; save iter
     
     ; prepare buffer
-    mov dword [buffer], 0   ; clear buffer
-    mov edi, buffer         ; recover destination
+    mov     dword [buffer], 0   ; clear buffer
+    mov     edi, buffer         ; recover destination
 
     ; copy part of source
-    mov ecx, [esp + 4]  ; how much bytes need to copy
+    mov     ecx, [esp + 4]  ; how much bytes need to copy
     cld
-    rep movsb           ; copy bytes
+    rep     movsb           ; copy bytes
     
     ; print value
-    mov [esp + 8], esi      ; save esi
-    mov esi, [buffer]       ; move edi into esi
-    call printInt
-    mov esi, [esp + 8]      ; recover esi
+    mov     [esp + 8], esi  ; save esi
+    mov     esi, [buffer]   ; move edi into esi
+    call    printInt
+    mov     esi, [esp + 8]  ; recover esi
 
     ; print tiny space between characters
-    mov eax, ' '
-    call printSym
+    mov     eax, ' '
+    call    printSym
 
     ; recover iter
-    mov ecx, [esp]
-    loop l_3
+    mov     ecx, [esp]
+    loop    l_3
 ; loop end
-    call printLF    ; print feedline
+    call    printLF    ; print feedline
 
     ; free memory and recover values
-    add esp, 12
-    pop ecx
-    pop ebx
-    pop edi
-    pop esi
+    add     esp, 12
+    pop     ecx
+    pop     ebx
+    pop     edi
+    pop     esi
     ret
 
 section .bss
@@ -219,7 +224,5 @@ section .bss
     buffer: resb 4
 
 section .data
-    ; source db   9, 8, 7, 6, 5, 44, 3, 2, 0, 1
-    ; source dw   9, 1880, 7, 66, 5, 44, 3, 2, 0, 1
-    source dd   2000, 50000, 3000, 4000, 0, 1, 6000, 80000, 9, 20
+    source dw   9, 1880, 7, 66, 5, 44, 3, 2, 0, 1
     source_byte_len equ $ - source
